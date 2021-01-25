b5cd35b4c469f06749c9508279f1d8049e48b50c
paras kothiyal <paraskothiyal@parass-MacBook-Pro.local>
2021-01-25 15:50:55 +0530

    Change done for spree 4.1

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,9 +1,7 @@
-Spree::LineItem.class_eval do
-
+module SpreeItemReturns::LineItemDecorator
   def is_returnable?
     return false unless product.returnable?
-    return true if product.return_time.zero?
-    Time.current < (order.completed_at + product.return_time.days)
+    return true
   end
-
 end
+Spree::LineItem.prepend SpreeItemReturns::LineItemDecorator
--- app/models/spree/return_item_decorator.rb
+++ app/models/spree/return_item_decorator.rb
@@ -1,5 +1,4 @@
-Spree::ReturnItem.class_eval do
-
+module SpreeItemReturns::ReturnItemDecorator
   def returnable?
     inventory_unit.variant.product.returnable?
   end
@@ -7,4 +6,6 @@ Spree::ReturnItem.class_eval do
   def returned?
     inventory_unit.shipped? && return_authorization.allow_return_item_changes? && !reimbursement
   end
+
 end
+Spree::ReturnItem.prepend SpreeItemReturns::ReturnItemDecorator
