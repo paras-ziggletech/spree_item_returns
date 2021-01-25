5a8f5b6d181339c9a1794f274505e98fc836e42e
paras kothiyal <paraskothiyal@parass-MacBook-Pro.local>
2021-01-25 17:08:04 +0530

    Add return history button to nav list

--- app/overrides/spree/return_authorization/user_return_authorization_history_button.rb
+++ app/overrides/spree/return_authorization/user_return_authorization_history_button.rb
@@ -1,9 +1,10 @@
 Deface::Override.new(
-  virtual_path: 'spree/users/order_account',
+  virtual_path: 'spree/shared/_user_account_nav',
   name: 'user_return_authorization_history_button',
-  insert_bottom: "[data-hook='account_my_orders'] h3",
-  text: "<%= link_to(spree.return_authorizations_path, class: 'btn btn-primary float-right') do %>
-    <span class='glyphicon glyphicon-list-alt'><%= icon(name: 'burger', classes: 'd-none d-xl-inline-block', width: 20, height: 20)  %></span>
-    <%= Spree.t(:returns_history) %>
-    <% end %>"
+  insert_bottom: "[data-hook='account_my_orders']",
+  text: "<li class='nav-item'>
+    <%= link_to(spree.return_authorizations_path, class: 'nav-link') do %>
+      <%= Spree.t(:returns_history) %>
+    <% end %>
+    </li>"
 )
