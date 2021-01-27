a98f00484d6749cadcf5269e5fbbd0e828008bcd
paras kothiyal <paraskothiyal@parass-MacBook-Pro.local>
2021-01-27 13:02:22 +0530

    Update icon for product return

--- app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
+++ app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
@@ -1,12 +1,11 @@
 Deface::Override.new(
   virtual_path: 'spree/users/order_account',
   name: 'user_return_authorization_initailization_button',
-  insert_bottom: ".order-show-number",
+  insert_bottom: "[data-hook='order-show-number']",
   text: "
         <% if spree_current_user.present? && order.shipped? && order.has_returnable_products? && order.has_returnable_line_items? %>
-          <%= link_to(spree.new_order_return_authorization_path(order.number), class: 'btn btn-primary float-right') do %>
-            <span class='glyphicon glyphicon-send'><%= icon(name: 'send', classes: 'd-none d-xl-inline-block', width: 25, height: 25)  %></span>
-            <%= Spree.t(:return_products) %>
+          <%= link_to(spree.new_order_return_authorization_path(order.number), class: 'btn float-right',title: 'Return Products') do %>
+            <span class='glyphicon glyphicon-refresh'><%= icon(name: 'refresh', classes: 'd-none d-xl-inline-block', width: 25, height: 25)  %></span>
           <% end %>
         <% end %>
         "
